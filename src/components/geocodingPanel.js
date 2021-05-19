76d153ec947927230955db9187e9ac1075136a76
cw2sid <annalisa.wilde@gmail.com>
2021-05-19 12:33:14 -0400

    added link to python script

--- src/components/geocodingPanel.js
+++ src/components/geocodingPanel.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { PieChart } from './pieChart.js'
 import { BarChart } from './barChart.js'
-import summarydata from '../data/summary_data'
 import * as d3 from 'd3'
 import top_10_countries from '../data/top_10_countries.csv'
 import { colors } from '../style/colors.js'
@@ -31,7 +29,8 @@ class GeocodingPanel extends React.Component {
                         Geocoding is the process where by addresses are resolved to longitude and latitude, that is: precise points on a map that
                         can be analyzed. In this project the addresses were first geocoded against the US Census Bureau's geocoding api, which is 
                         free to use but very slow. Because of the large volume (639,722) of addresses, multithreading was used to speed up the process.
-                        Even with six processes running it took over 22 hours to complete the geocoding. An example of the script used can be found here. <br/><br/>
+                        Even with six processes running it took over 22 hours to complete the geocoding. An example of the script 
+                        used can be found <a className='light-link' href="https://raw.githubusercontent.com/rawild/ny-donors-mapped/main/src/data/geocode_census.py">here</a>. <br/><br/>
                         After the US Census geocoding, there were still 95,630 donor addresses that could not be resolved to an geographic coordinate. 
                         These addresses were then geocoded against the HERE geocoder. After this, all but 2,127 were resolved. As shown above, that means 
                         99.7% of the donors were able to be geocoded. The missing donors contributed $751,881, less than .2% of the total.
